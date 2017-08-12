b8979048b17412b1b769b21b51cf1e48e7ac7237
kchrusciel <krzysztof.chrusciel@outlook.com>
2017-08-12 12:48:56 +0200

    Small refactoring

--- TheBowlingGame/src/test/java/pl/codecouple/BowlingGameTest.java
+++ TheBowlingGame/src/test/java/pl/codecouple/BowlingGameTest.java
@@ -80,15 +80,16 @@ public class BowlingGameTest {
         int[] rolls = new int[21];
         int roll = 0;
 
+        int index = 0;
+
         int getScore() {
             int score = 0;
-            int index = 0;
             for(int frame = 0; frame<10; frame++){
-                if(rolls[index] == 10) { //strike
+                if(isStrike()) {
                     score += 10 + rolls[index+1] + rolls[index+2];
                     index++;
                 }
-                else if(rolls[index] + rolls[index+1] == 10){ //spare
+                else if(isSpare()){
                     score += 10 + rolls[index+2];
                     index+=2;
                 } else{
@@ -97,6 +98,14 @@ public class BowlingGameTest {
                 }
             }
             return score;
+        }
+
+        private boolean isSpare() {
+            return rolls[index] + rolls[index+1] == 10;
+        }
+
+        private boolean isStrike() {
+            return rolls[index] == 10;
         }
 
         void roll(int pins) {
