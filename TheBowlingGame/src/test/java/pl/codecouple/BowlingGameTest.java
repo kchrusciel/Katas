ffb768359c12e672b8bf160e471d830be6294429
kchrusciel <krzysztof.chrusciel@outlook.com>
2017-08-12 12:19:11 +0200

    Add second condition

--- TheBowlingGame/src/test/java/pl/codecouple/BowlingGameTest.java
+++ TheBowlingGame/src/test/java/pl/codecouple/BowlingGameTest.java
@@ -11,26 +11,44 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BowlingGameTest {
 
-    //Should return zero as score when all rolls are missed
+    //Should return zero as a score when all rolls are missed
 
     @Test
     public void shouldReturnZeroWhenAllRollsAreMissed() throws Exception {
         // Given
         BowlingGame game = new BowlingGame();
         // When
-        IntStream.range(0,20).forEach(game::roll);
+        IntStream.range(0,20).forEach(roll->game.roll(0));
         int score = game.getScore();
         // Then
         assertThat(score).isEqualTo(0);
     }
 
+    //Should return 20 as a score when you knock down one pin per roll
+
+
+    @Test
+    public void shouldReturnTwentyWhenKnockDownOnePinPerRoll() throws Exception {
+        // Given
+        BowlingGame game = new BowlingGame();
+        // When
+        IntStream.range(0,20).forEach(roll->game.roll(1));
+        int score = game.getScore();
+        // Then
+        assertThat(score).isEqualTo(20);
+
+    }
+
     private class BowlingGame {
+
+        private int score = 0;
+
         int getScore() {
-            return 0;
+            return score;
         }
 
         void roll(int roll) {
-
+            score+=roll;
         }
     }
 }
