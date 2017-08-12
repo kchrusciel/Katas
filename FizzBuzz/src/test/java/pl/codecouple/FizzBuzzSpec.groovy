ed9db34410fd3eae722b9f2914ad0c107e60b835
kchrusciel <krzysztof.chrusciel@outlook.com>
2017-08-12 10:06:41 +0200

    Add buzz spock

--- FizzBuzz/src/test/java/pl/codecouple/FizzBuzzSpec.groovy
+++ FizzBuzz/src/test/java/pl/codecouple/FizzBuzzSpec.groovy
@@ -1,5 +1,6 @@
 package pl.codecouple
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 /**
@@ -7,19 +8,30 @@ import spock.lang.Specification
  */
 class FizzBuzzSpec extends Specification {
 
+    @Shared
+    def fizzBuzz = new FizzBuzz()
+
     def "Should return 'Fizz' when number is divided by three" (){
-        given:
-            def fizzBuzz = new FizzBuzz()
         when:
             def result = fizzBuzz.check(3)
         then:
             result == "Fizz"
     }
 
+    def "Should return 'Buzz' when number is divided by five" (){
+        when:
+            def result = fizzBuzz.check(5)
+        then:
+            result == "Buzz"
+    }
+
     class FizzBuzz{
 
         String check(int number) {
-            "Fizz"
+            if(number%3==0){
+                return "Fizz"
+            }
+            return "Buzz"
         }
     }
 
